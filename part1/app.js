38435523ddd0702494b209911b4f59497a88c435
Lunnn824 <141891043+Lunnn824@users.noreply.github.com>
2025-06-20 16:14:41 +0000

    [automatic commit] 2025-06-20 16:14:41 UTC

--- part1/app.js
+++ part1/app.js
@@ -4,6 +4,8 @@ const pool    = require('./db');
 const app  = express();
 const port = 8080;
 
+
+
 /* ---------- Mount the three routes ---------- */
 app.use('/api/dogs',         require('./routes/dogs'));
 app.use('/api/walkrequests', require('./routes/walkrequests'));
