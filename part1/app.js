6c3ca5c324483b881e600f569e428f7b9669acc2
Lunnn824 <141891043+Lunnn824@users.noreply.github.com>
2025-06-20 11:38:24 +0000

    [automatic commit] 2025-06-20 11:38:24 UTC

---
+++ part1/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const pool    = require('./db');
+
+const app  = express();
+const port = 8080;
+
+/* ---------- Mount the three routes ---------- */
+app.use('/api/dogs',         require('./routes/dogs'));
+app.use('/api/walkrequests', require('./routes/walkrequests'));
+app.use('/api/walkers',      require('./routes/walkers'));
+
+/* ---------- Start server ---------- */
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
