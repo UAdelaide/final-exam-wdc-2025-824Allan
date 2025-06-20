83d929360cdf839e93575a5a86840cf1bb9b02a3
Lunnn824 <141891043+Lunnn824@users.noreply.github.com>
2025-06-20 11:33:14 +0000

    [automatic commit] 2025-06-20 11:33:14 UTC

--- part1/db.js
+++ part1/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  database: 'DogWalkService',   // 你的数据库名
+  database: 'DogWalkService',
   waitForConnections: true,
 });
 
