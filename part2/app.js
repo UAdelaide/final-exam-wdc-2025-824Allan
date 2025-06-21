e23607ee9923ecaf496326b4b946ce7910fe85ff
Lunnn824 <141891043+Lunnn824@users.noreply.github.com>
2025-06-21 02:03:28 +0000

    [automatic commit] 2025-06-21 02:03:28 UTC

--- part2/app.js
+++ part2/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({wxtended:false}));
+app.use(express.urlencoded({wxtended:false}));   // Parsing the form
 
 app.use(express.static(path.join(__dirname, '/public')));
 
