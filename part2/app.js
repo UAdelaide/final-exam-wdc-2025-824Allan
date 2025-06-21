a4c0edb34968d4734d74763aa236676dd32ad9a1
Lunnn824 <141891043+Lunnn824@users.noreply.github.com>
2025-06-21 03:33:23 +0000

    [automatic commit] 2025-06-21 03:33:22 UTC

--- part2/app.js
+++ part2/app.js
@@ -7,20 +7,23 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
-
+app.use(express.urlencoded({wxtended:false}));   // Parsing the form
 app.use(session({
-  secret: 'secretkey',
-  resave: false,
-  saveUninitialized: true
+    secret: 'dogwalk-secret',
+    resave: false,
+    saveUninitialized: false
 }));
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/',          authRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
