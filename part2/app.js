4b16aee6a56bd5eda8668baf4dd1ecbc726ea880
Lunnn824 <141891043+Lunnn824@users.noreply.github.com>
2025-06-21 02:07:19 +0000

    [automatic commit] 2025-06-21 02:07:19 UTC

--- part2/app.js
+++ part2/app.js
@@ -19,10 +19,11 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use()
 
 // Export the app instead of listening here
 module.exports = app;
